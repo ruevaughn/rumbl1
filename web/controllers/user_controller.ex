f04584b42645c74f31813c0831bb7f5c869b9b3c
Chase Jensen <soccerchase@gmail.com>
2016-12-28 17:38:12 -0700

    User Show, pg 49

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,11 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
 
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Rumbl.User, id)
+    render conn, "show.html", user: user
+  end
+
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
     render conn, "index.html", users: users
