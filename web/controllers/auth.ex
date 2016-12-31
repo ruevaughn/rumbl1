87f1ec5ef53cdcb6383a229536bf33c98eb933de
Chase Jensen <soccerchase@gmail.com>
2016-12-31 14:20:56 -0700

    Added comeonin, authentication plug, finished authentication

---
+++ web/controllers/auth.ex
@@ -0,0 +1,46 @@
+defmodule Rumbl.Auth do
+  import Plug.Conn
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
+  def init(opts) do
+    Keyword.fetch!(opts, :repo)
+  end
+
+  def call(conn, repo) do
+    # I think this still IS functional, because even though we're assigning a variable, it's never
+    # being changed, only used for pattern matching. Variable assignment if it was never used
+    # your code would be a lot of long one liners.
+    user_id = get_session(conn, :user_id)
+    user = user_id && repo.get(Rumbl.User, user_id)
+    assign(conn, :current_user, user)
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
+  def login_by_username_and_pass(conn, username, given_pass, opts) do
+    repo = Keyword.fetch!(opts, :repo)
+    user = repo.get_by(Rumbl.User, username: username)
+
+    cond do
+      user && checkpw(given_pass, user.password_hash) ->
+        {:ok, login(conn, user)}
+      user ->
+        {:error, :unauthorized, conn}
+      true ->
+        # When a user isn’t found, we use comeonin’s dummy_checkpw() function to simulate a password check with variable timing. This hardens our authentication layer against timing attacks
+        dummy_checkpw()
+        {:error, :not_found, conn}
+    end
+  end
+
+  def logout(conn) do
+    configure_session(conn, drop: true)
+  end
+
+
+end
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,17 +2,21 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
 
+  plug :authenticate when action in [:index, :show]
+
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
        render(conn, "new.html", changeset: changeset)
     end
   end
+
 
   def show(conn, %{"id" => id}) do
     user = Repo.get(User, id)
@@ -27,5 +31,16 @@ defmodule Rumbl.UserController do
   def index(conn, _params) do
     users = Repo.all(User)
     render conn, "index.html", users: users
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
